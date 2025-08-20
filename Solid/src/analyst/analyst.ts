f15b3a57e6cc1001631e321114c2267b5f739ee8
pooyan <thepooyan@gmail.com>
2025-08-20 14:02:48 +0330

    start

---
+++ Solid/src/analyst/analyst.ts
@@ -0,0 +1,48 @@
+import fs from "fs"
+import {JSDOM} from "jsdom"
+import pages from "~/json/url.json"
+
+const EXCLUDED_SCRIPTS = [
+    "https://www.googletagmanager.com/gtag/js?id=G-N901M1ZKXX",
+]
+
+const main = async () => {
+
+    const urlBase = "https://www.tahlildadeh.com"
+
+    type result = {
+        name: string,
+        url: string,
+        css: string[],
+        js: string[]
+    }
+
+    let results:result[] = []
+
+    const request = (url: string) => {
+        return fetch(urlBase + url)
+    }
+
+    const proccessBody = (name:string, url:string, body: string) => {
+        let dom = new JSDOM(body)
+
+        let links = dom.window.document.getElementsByTagName("link")
+        let css = Array.from(links).filter(l => l.rel === "stylesheet").map(l => l.href)
+
+        let scripts = dom.window.document.getElementsByTagName("script")
+        let js = Array.from(scripts).filter(l => l.src !== "" && !EXCLUDED_SCRIPTS.includes(l.src)).map(l => l.src)
+
+        results.push({name, url,js, css})
+    }
+
+    for (const {name,url} of pages) {
+        let result = await request(url)
+        let body = await result.text()
+        proccessBody(name, url, body)
+    }
+
+    fs.writeFile("./result.json", JSON.stringify(results, null, "\t"), (err) => {
+        console.log(err)
+    })
+}
+main()
---
+++ Solid/src/routes/index.tsx
@@ -0,0 +1,40 @@
+import result from "~/json/result.json"
+
+export default function Home() {
+  const allCss = result.map(r => r.css).flat()
+  const alluniqeCss = new Set(allCss)
+
+  const alljs = result.map(r => r.js).flat()
+  const allUniqeJs = new Set(alljs)
+
+  return (
+    <main class="p-5 bg-zinc-300 min-h-dvh text-xl">
+        <h1 class="text-3xl mb-5">Tahlildadeh.com</h1>
+        <div class="border-1 border-black p-5 rounded mb-5 grid grid-cols-2">
+          <h2 class="mb-5">
+            Every Css used in site:
+          </h2>
+          <h2>
+            Every Js used in site:
+          </h2>
+          {Array.from(alluniqeCss).map(u => <div class="text-zinc-800 text-sm">
+            {u}
+          </div>)}
+        </div>
+        <div>
+          {result.map(r => 
+            <div class="border-1 border-zinc-700 p-5 rounded mb-2 overflow-hidden"> 
+              <p>{r.name}</p>
+              <a class="text-blue-800" href={`https://tahlildadeh.com${r.url}`} target="_blank">{r.url}</a>
+              <div>
+                  css:
+                  {r.css.map(c => <p class="text-zinc-600 text-sm">{c}</p>)}
+                  js:
+                  {r.js.map(c => <p class="text-zinc-600 text-sm">{c}</p>)}
+              </div>
+            </div>
+          )}
+        </div>
+    </main>
+  )
+}
