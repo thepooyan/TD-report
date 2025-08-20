7bd8c037c7f76db00e85be34a1d0ad4cb490dde3
pooyan <thepooyan@gmail.com>
2025-08-20 17:14:31 +0330

    l

--- Solid/src/app.tsx
+++ Solid/src/app.tsx
@@ -1,4 +1,4 @@
-import { Router } from "@solidjs/router";
+import { A, Router } from "@solidjs/router";
 import { FileRoutes } from "@solidjs/start/router";
 import { Suspense } from "solid-js";
 import "./app.css";
@@ -8,6 +8,7 @@ export default function App() {
     <Router
       root={props => (
         <div class="p-5 bg-zinc-300 min-h-dvh text-xl">
+          <A href="/" class="p-2 bg-yellow-300 rounded mb-5 block w-max ml-auto">Main Page</A>
           <Suspense>{props.children}</Suspense>
         </div>
       )}
