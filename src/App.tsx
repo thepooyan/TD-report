d7bce50895662100ade4951d390d66d0720da9f5
pooyan <thepooyan@gmail.com>
2025-08-20 13:37:29 +0330

    sldkfj

--- src/App.tsx
+++ src/App.tsx
@@ -5,9 +5,23 @@ const App = () => {
   const allCss = result.map(r => r.css).flat()
   const alluniqeCss = new Set(allCss)
 
+  const alljs = result.map(r => r.js).flat()
+  const allUniqeJs = new Set(alljs)
+
   return (
-    <main class="p-5 bg-zinc-300 min-h-dvh text-xl  ">
+    <main class="p-5 bg-zinc-300 min-h-dvh text-xl">
         <h1 class="text-3xl mb-5">Tahlildadeh.com</h1>
+        <div class="border-1 border-black p-5 rounded mb-5 grid grid-cols-2">
+          <h2 class="mb-5">
+            Every Css used in site:
+          </h2>
+          <h2>
+            Every Js used in site:
+          </h2>
+          {Array.from(alluniqeCss).map(u => <div class="text-zinc-800 text-sm">
+            {u}
+          </div>)}
+        </div>
         <div>
           {result.map(r => 
             <div class="border-1 border-zinc-700 p-5 rounded mb-2 overflow-hidden"> 
