feada5d28ded651308a35ac4989ee3d427c1c4e7
pooyan <thepooyan@gmail.com>
2025-08-21 14:44:03 +0330

    done

--- Solid/src/routes/bundle/[type]/[name].tsx
+++ Solid/src/routes/bundle/[type]/[name].tsx
@@ -4,6 +4,7 @@ import pages from "~/json/result.json"
 import bundleInfo from "~/json/bundles.json"
 import Pages from "~/components/Pages"
 import BundleLink from "~/components/BundleLink"
+import sassGraph from "~/json/sass-graph.json"
 
 const js = () => {
     const params = useParams<{type: "css" | "js", name: string}>()
@@ -17,25 +18,42 @@ const js = () => {
     }
     let isBundle = () => fileName().match(/\?v=.*$/);
 
-  
+    const lowercaseSassGraph = Object.fromEntries(
+      Object.entries(sassGraph).map(([key, value]) => [key.toLowerCase(), value])
+    );
+
     const bundleItem = () => bundleInfo.find(b => b.bundleName.toLowerCase() === cleanName().toLowerCase())
+    const graphItem = () => {
+      let name = cleanName().toLocaleLowerCase()
+      if (name.endsWith(".min.css")) name = name.replace(".min.css", ".scss")
+      if (name in lowercaseSassGraph) return lowercaseSassGraph[name as keyof typeof sassGraph]
+      return null
+    }
 
   return (
     <div class="space-y-5">
         <div class="border-1 border-black p-5 rounded">
             {isBundle() && <div class="bg-blue-300 rounded p-2 w-max inline-block mr-5">
-              bundle name: 
+              Bundle name: 
             </div> || <div class="bg-green-300 rounded p-2 w-max inline-block mr-5">
-                file name:
+                File name:
             </div>}
             <span class="text-bold">{cleanName()}</span>
             <div>
             <Show when={bundleItem()}>
                 {b => <>
                     <p class="  my-2">
-                    Inner Files:
+                    Bundle Files:
                     </p>
                     {b().bundleItems.map(b => <BundleLink type={params.type} u={b}/>)}
+              </>}
+            </Show>
+            <Show when={graphItem()}>
+              {g => <>
+                <p class="  my-2">
+                  SASS imports:
+                </p>
+                {g().map(str => <BundleLink type="css" u={str}/>)}
               </>}
             </Show>
             </div>
