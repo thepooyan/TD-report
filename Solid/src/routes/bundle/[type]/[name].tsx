72288c9648978d74c08788471f258dba07d3f49d
pooyan <thepooyan@gmail.com>
2025-08-21 17:34:49 +0330

    nothing

--- Solid/src/routes/bundle/[type]/[name].tsx
+++ Solid/src/routes/bundle/[type]/[name].tsx
@@ -21,10 +21,18 @@ const js = () => {
     const bundleChildren = () => {
       return graph.bunlde.get(cleanName().toLowerCase(), "children")
     }
+    const bundleParents = () => {
+      return graph.bunlde.get(cleanName().toLocaleLowerCase(), "parents")
+    }
     const sassChildren = () => {
       let name = cleanName().toLocaleLowerCase()
       if (name.endsWith(".min.css")) name = name.replace(".min.css", ".scss")
       return graph.sass.get(name, "children")
+    }
+    const sassParents = () => {
+      let name = cleanName().toLocaleLowerCase()
+      if (name.endsWith(".min.css")) name = name.replace(".min.css", ".scss")
+      return graph.sass.get(name, "parents")
     }
 
   return (
@@ -36,26 +44,46 @@ const js = () => {
                 File name:
             </div>}
             <span class="text-bold">{cleanName()}</span>
-            <div>
-            <Show when={bundleChildren()}>
-                {b => <>
+            <div class="grid grid-cols-2">
+              <div>
+                <Show when={bundleChildren()}>
+                    {b => <>
+                        <p class="  my-2">
+                        Bundle children:
+                        </p>
+                        {b().map(b => <BundleLink type={params.type} u={b}/>)}
+                  </>}
+                </Show>
+                <Show when={sassChildren()}>
+                  {g => <>
                     <p class="  my-2">
-                    Bundle Files:
+                      SASS children:
                     </p>
-                    {b().map(b => <BundleLink type={params.type} u={b}/>)}
-              </>}
-            </Show>
-            <Show when={sassChildren()}>
-              {g => <>
-                <p class="  my-2">
-                  SASS imports:
-                </p>
-                {g().map(str => <BundleLink type="css" u={str}/>)}
-              </>}
-            </Show>
+                    {g().map(str => <BundleLink type="css" u={str}/>)}
+                  </>}
+                </Show>
+              </div>
+              <div>
+                <Show when={bundleParents()}>
+                    {b => <>
+                        <p class="  my-2">
+                        Bundle parents:
+                        </p>
+                        {b().map(b => <BundleLink type={params.type} u={b}/>)}
+                  </>}
+                </Show>
+                <Show when={sassParents()}>
+                  {g => <>
+                    <p class="  my-2">
+                      SASS parents:
+                    </p>
+                    {g().map(str => <BundleLink type="css" u={str}/>)}
+                  </>}
+                </Show>
+              </div>
             </div>
         </div>
-        <h2>Used in:</h2>
+        <h2>Used in Pages:</h2>
         <Pages pages={() => pages.filter(r => r[params.type].includes(fileName()) )} highlight={() => fileName()}/>
     </div>
   )
